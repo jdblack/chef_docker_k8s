383ca5096c7bb813b9be214746b92cead1798eac
James Blackwell <jblack@linuxguru.net>
2020-07-04 19:17:13 -0700

    integrate es

--- files/chef-server.rb
+++ files/chef-server.rb
@@ -1,9 +1,24 @@
 api_fqdn ENV['CHEF_FQDN']
+
+# Use the copy of the certs that we dropped from secrets
 nginx['ssl_certificate']="/etc/opscode/certs/tls.crt"
 nginx['ssl_certificate_key']="/etc/opscode/certs/tls.key"
+
+
+# Externalize the database
 postgresql['db_superuser'] = ENV['POSTGRES_USER']
 postgresql['db_superuser_password'] = ENV['POSTGRES_PASSWORD']
 postgresql['external'] = true
 postgresql['vip'] = ENV['POSTGRES_FQDN']
 
+# We use the db instead of the localfilesystem because our
+# frontends are transient.  S3 is a better option if you have it!
+bookshelf['storage_type'] = :sql
+
+# We rely on the external ES host for search
+opscode_solr4['external'] = true
+opscode_solr4['external_url'] = 'eshost'
+opscode_erchef['search_provider'] = ENV['ELASTICSEARCH_FQDN'] + ":9200"
+opscode_erchef['search_queue_mode'] = 'batch' # es requires batch
+opscode_expander['enable'] = false # expander doesnt work in batch mode
 
